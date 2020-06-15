fce2675a860f787cf5322a116ad9f1bcc9fc4a10
Gordon home PC <aardappel156@gmail.com>
2020-06-15 17:37:09 +0200

    added jumppad and updated map

---
+++ PaperPrototype/Assets/Scripts/Element.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    public int water;
+    public int nature;
+    public int fire;
+    
+}
---
+++ PaperPrototype/Assets/Scripts/PlantJumpad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantJumpad : MonoBehaviour
+{
+    public bool activated = false;
+    public float jumpForce;
+
+    public void ActivateJumpad()
+    {
+        // activate if shooter has high enough water energy
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("toched player");
+            other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
